83c3249a719c4c288d398e0e3cee49cc563fb710
Phillipp Glanz <p.glanz@madfix.me>
2022-12-02 00:31:42 +0100

    First working function

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.7.20"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "net.onelitefeather"
@@ -7,22 +8,51 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://jitpack.io")
+    maven {
+        val groupdId = 28 // Gitlab Group
+        url = if (System.getenv().containsKey("CI")) {
+            val ciApiv4Url = System.getenv("CI_API_V4_URL")
+            uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
+        } else {
+            uri("https://gitlab.themeinerlp.dev/api/v4/groups/$groupdId/-/packages/maven")
+        }
+        name = "GitLab"
+        credentials(HttpHeaderCredentials::class.java) {
+            name = if (System.getenv().containsKey("CI")) {
+                "Job-Token"
+            } else {
+                "Private-Token"
+            }
+            value = if (System.getenv().containsKey("CI")) {
+                System.getenv("CI_JOB_TOKEN")
+            } else {
+                val gitLabPrivateToken: String? by project
+                println(gitLabPrivateToken)
+                gitLabPrivateToken
+            }
+        }
+        authentication {
+            create<HttpHeaderAuthentication>("header")
+        }
+    }
+
 }
 
 dependencies {
-    // Paper
-    compileOnly(libs.paper)
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    compileOnly("com.github.Minestom:Minestom:master-SNAPSHOT")
+    implementation("net.kyori:adventure-text-minimessage:4.12.0")
+    // compileOnly("de.icevizion.lib:Aves:1.2.0+6d3788cb")
+    api("com.squareup.moshi:moshi:1.14.0")
+    api("com.squareup.moshi:moshi-kotlin:1.14.0")
+    api("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+
+
 }
 
 tasks {
-    compileJava {
-        options.release.set(17)
-        options.encoding = "UTF-8"
+    shadowJar {
+        archiveFileName.set("${rootProject.name}.${archiveExtension.getOrElse("jar")}")
     }
 }
 
