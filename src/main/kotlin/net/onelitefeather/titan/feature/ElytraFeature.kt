7b1b7d088087cca8c2d33010b5ea1ce642b510e3
Phillipp Glanz <p.glanz@madfix.me>
2022-12-02 00:31:42 +0100

    First working function

---
+++ src/main/kotlin/net/onelitefeather/titan/feature/ElytraFeature.kt
@@ -0,0 +1,34 @@
+package net.onelitefeather.titan.feature
+
+import net.minestom.server.event.Event
+import net.minestom.server.event.EventNode
+import net.minestom.server.event.player.PlayerStartFlyingWithElytraEvent
+import net.minestom.server.event.player.PlayerStopFlyingWithElytraEvent
+import net.minestom.server.event.player.PlayerUseItemEvent
+import net.minestom.server.item.ItemStack
+import net.onelitefeather.titan.service.firework
+
+class ElytraFeature(eventNode: EventNode<Event>) {
+
+    init {
+        eventNode.addListener(PlayerStartFlyingWithElytraEvent::class.java, this::playerStartFlyingWithElytraEvent)
+        eventNode.addListener(PlayerStopFlyingWithElytraEvent::class.java, this::playerStopFlyingWithElytraEvent)
+        eventNode.addListener(PlayerUseItemEvent::class.java, this::playerUseItemEvent)
+    }
+
+    private fun playerStartFlyingWithElytraEvent(event: PlayerStartFlyingWithElytraEvent) {
+        event.player.inventory.setItemStack(0, firework)
+    }
+
+    private fun playerStopFlyingWithElytraEvent(event: PlayerStopFlyingWithElytraEvent) {
+        event.player.inventory.setItemStack(0, ItemStack.AIR)
+    }
+
+    private fun playerUseItemEvent(event: PlayerUseItemEvent) {
+        val item = event.itemStack
+        if (item == firework && event.player.isFlyingWithElytra) {
+            event.player.velocity = event.player.velocity.normalize().add(event.player.position.direction().mul(35.0))
+        }
+    }
+
+}
