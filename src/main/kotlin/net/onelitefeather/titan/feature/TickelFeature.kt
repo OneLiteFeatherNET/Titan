7fe6396f28e4c5642c56087d68acf2a8c0470d41
TheMeinerLP <p.glanz@madfix.me>
2023-08-11 22:52:11 +0200

    Cleanup code. Remove useless code, fix tickle feature and use better usage of component system.

--- src/main/kotlin/net/onelitefeather/titan/feature/TickelFeature.kt
+++ src/main/kotlin/net/onelitefeather/titan/feature/TickelFeature.kt
@@ -1,6 +1,8 @@
 package net.onelitefeather.titan.feature
 
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import net.minestom.server.entity.Player
 import net.minestom.server.event.Event
 import net.minestom.server.event.EventListener
@@ -8,7 +10,7 @@ import net.minestom.server.event.EventNode
 import net.minestom.server.event.entity.EntityAttackEvent
 import net.minestom.server.item.Material
 import net.minestom.server.tag.Tag
-import net.onelitefeather.titan.service.brodcastMessage
+import net.onelitefeather.titan.TICKLE_MESSAGE
 
 class TickelFeature(eventNode: EventNode<Event>) {
 
@@ -24,11 +26,16 @@ class TickelFeature(eventNode: EventNode<Event>) {
         if (player is Player && target is Player) {
             player.instance ?: return
             if (player.itemInOffHand.material() == Material.FEATHER || player.itemInMainHand.material() == Material.FEATHER) {
-                if (player.hasTag(cooldown) && System.currentTimeMillis() > player.getTag(cooldown)) {
+                if (!player.hasTag(cooldown)) {
                     player.setTag(cooldown, System.currentTimeMillis() + 4000)
-                    player.instance?.brodcastMessage(MiniMessage.miniMessage().deserialize(
-                        "<yellow>${player.username} <white>tickled <yellow>${target.username}"
-                    ))
+                    player.instance?.players?.forEach { worldPlayer ->
+                        val message = MiniMessage.miniMessage().deserialize(
+                            TICKLE_MESSAGE, Placeholder.component("player", player.displayName ?: player.name), Placeholder.component("target", target.displayName ?: target.name)
+                        )
+                        worldPlayer.sendMessage(message)
+                    }
+                } else if (System.currentTimeMillis() > player.getTag(cooldown)) {
+                    player.removeTag(cooldown)
                 }
             }
         }
