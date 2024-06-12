8b5c4bbffdc7c839ada74f3b983adc0175c70f63
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-06-12 20:08:18 +0200

    Improve code quality

--- src/main/kotlin/net/onelitefeather/titan/feature/TickelFeature.kt
+++ src/main/kotlin/net/onelitefeather/titan/feature/TickelFeature.kt
@@ -8,6 +8,7 @@ import net.minestom.server.event.EventListener
 import net.minestom.server.event.EventNode
 import net.minestom.server.event.entity.EntityAttackEvent
 import net.minestom.server.item.Material
+import net.minestom.server.network.packet.server.play.SetCooldownPacket
 import net.minestom.server.tag.Tag
 
 class TickelFeature(eventNode: EventNode<Event>) {
@@ -26,6 +27,10 @@ class TickelFeature(eventNode: EventNode<Event>) {
             if (player.itemInOffHand.material() == Material.FEATHER || player.itemInMainHand.material() == Material.FEATHER) {
                 if (!player.hasTag(cooldown)) {
                     player.setTag(cooldown, System.currentTimeMillis() + 4000)
+                    val packet = SetCooldownPacket(player.itemInOffHand.material().id(),
+                        ((System.currentTimeMillis() + 4000) / 20).toInt()
+                    )
+                    player.playerConnection.sendPacket(packet)
                     player.instance?.players?.forEach { worldPlayer ->
                         val message = MiniMessage.miniMessage().deserialize(
                             TICKLE_MESSAGE, Placeholder.component("player", player.displayName ?: player.name), Placeholder.component("target", target.displayName ?: target.name)
