ff32ac9cfae7594e70997564bf54a4b649ceaf8d
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-06-12 19:18:25 +0200

    Improve build logic and update microtus support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,28 +9,44 @@ pluginManagement {
 
 
 dependencyResolutionManagement {
+    if (System.getenv("CI") != null) {
+        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
+        repositories {
+            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
+            maven {
+                val groupdId = 28 // Gitlab Group
+                val ciApiv4Url = System.getenv("CI_API_V4_URL")
+                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
+                name = "GitLab"
+                credentials(HttpHeaderCredentials::class.java) {
+                    name = "Job-Token"
+                    value = System.getenv("CI_JOB_TOKEN")
+                }
+                authentication {
+                    create<HttpHeaderAuthentication>("header")
+                }
+            }
+        }
+    }
+
     versionCatalogs {
         create("libs") {
-            version("minestom", "1.1.3")
+            version("microtus-bom", "1.4.3-SNAPSHOT")
             version("publishdata", "1.4.0")
-            version("shadow", "8.1.1")
-            version("adventure", "4.14.0")
             version("aves", "1.3.1+48463bee")
-            version("guava", "32.1.2-jre")
             version("cloudnet", "4.0.0-RC9")
 
             // Minestom
-            library("minestom", "net.onelitefeather.microtus", "Minestom").versionRef("minestom")
-            library("adventure.minimessage", "net.kyori", "adventure-text-minimessage").versionRef("adventure")
+            library("microtus-bom", "net.onelitefeather.microtus", "bom").versionRef("microtus-bom")
+            library("microtus", "net.onelitefeather.microtus", "Microtus").withoutVersion()
             library("aves", "de.icevizion.lib", "Aves").versionRef("aves")
-            library("guava", "com.google.guava", "guava").versionRef("guava")
 
             library("cloudnet.bridge", "eu.cloudnetservice.cloudnet", "bridge").versionRef("cloudnet")
             library("cloudnet.wrapper-jvm", "eu.cloudnetservice.cloudnet", "wrapper-jvm").versionRef("cloudnet")
             library("cloudnet.driver", "eu.cloudnetservice.cloudnet", "driver").versionRef("cloudnet")
 
             plugin("publishdata", "de.chojo.publishdata").versionRef("publishdata")
-            plugin("shadow", "com.github.johnrengelman.shadow").versionRef("shadow")
         }
     }
 }
