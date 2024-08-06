bf5a07423227f6d8df0ffdb5283ccaec3e7ac07a
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-08-06 20:36:22 +0200

    Add feature flag to activate Entity loading

--- src/main/kotlin/net/onelitefeather/titan/function/WorldFunction.kt
+++ src/main/kotlin/net/onelitefeather/titan/function/WorldFunction.kt
@@ -2,20 +2,31 @@ package net.onelitefeather.titan.function
 
 import com.google.inject.Inject
 import com.google.inject.name.Named
+import java.nio.file.Path
+import java.nio.file.Paths
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
+import net.minestom.server.instance.AnvilLoader
+import net.minestom.server.instance.IChunkLoader
 import net.minestom.server.instance.InstanceContainer
 import net.minestom.server.utils.chunk.ChunkUtils
 import net.onelitefeather.titan.instance.EntityAnvilLoader
 import net.onelitefeather.titan.utils.TitanFeatures
-import java.nio.file.Path
-import java.nio.file.Paths
 
 class WorldFunction : TitanFunction {
 
     private val basePath: Path = Paths.get("worlds")
     private val lobbyInstance = MinecraftServer.getInstanceManager()
-        .createInstanceContainer(EntityAnvilLoader(createWorldPath()))
+        .createInstanceContainer(requestAnvilLoader())
+
+    private fun requestAnvilLoader(): IChunkLoader {
+        return if (TitanFeatures.ENTITIES.isActive) {
+            EntityAnvilLoader(createWorldPath())
+        } else {
+            AnvilLoader(createWorldPath())
+        }
+    }
+
 
     @Named("spawnPos")
     @Inject
