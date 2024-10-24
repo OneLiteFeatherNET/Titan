021a719fb0b78caf8e471c65a44863f30073e54e
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-10-24 14:57:04 +0200

    [#19] Convert blockhandler from kotlin to java

---
+++ src/main/java/net/onelitefeather/titan/blockhandler/JukeboxHandler.java
@@ -0,0 +1,16 @@
+package net.onelitefeather.titan.blockhandler;
+
+import net.kyori.adventure.key.Key;
+import net.minestom.server.instance.block.BlockHandler;
+import net.minestom.server.utils.NamespaceID;
+import org.jetbrains.annotations.NotNull;
+
+public final class JukeboxHandler implements BlockHandler {
+
+    private static final @NotNull NamespaceID NAMESPACE_ID = NamespaceID.from(Key.key("minecraft:jukebox"));
+
+    @Override
+    public @NotNull NamespaceID getNamespaceId() {
+        return NAMESPACE_ID;
+    }
+}
--- src/main/kotlin/net/onelitefeather/titan/blockhandler/banner/BannerPattern.kt
+++
@@ -1,3 +0,0 @@
-package net.onelitefeather.titan.blockhandler.banner
-
-data class BannerPattern(val color: Int, val pattern: String)
