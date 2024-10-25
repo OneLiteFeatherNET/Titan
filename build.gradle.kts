6a7e38af5b7ff46e13a8f069122f55b062628703
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-10-25 19:28:16 +0200

    [#20] Implement docker publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import org.gradle.language.nativeplatform.internal.Dimensions
+
 plugins {
     java
+    application
     alias(libs.plugins.publishdata)
     `maven-publish`
-    id("io.github.goooler.shadow") version "8.1.8"
-
+    id("com.gradleup.shadow") version "8.3.3"
 }
 
 group = "net.onelitefeather.titan"
@@ -47,6 +49,9 @@ tasks {
     shadowJar {
         archiveClassifier.set("")
     }
+}
+application {
+    mainClass.set("net.onelitefeather.titan.TitanApplication")
 }
 
 publishData {
