47076649af58e1be0567f70c8048f08bcf3527de
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-10-28 14:55:59 +0100

    [#21] Fix flying with elytra

--- app/src/main/java/net/onelitefeather/titan/app/listener/ElytraStartFlyingListener.java
+++ app/src/main/java/net/onelitefeather/titan/app/listener/ElytraStartFlyingListener.java
@@ -1,21 +1,14 @@
 package net.onelitefeather.titan.app.listener;
 
 import net.minestom.server.event.player.PlayerStartFlyingWithElytraEvent;
-import net.onelitefeather.titan.common.config.AppConfig;
 import net.onelitefeather.titan.common.utils.Items;
 
 import java.util.function.Consumer;
 
 public final class ElytraStartFlyingListener implements Consumer<PlayerStartFlyingWithElytraEvent> {
-
-    private final AppConfig appConfig;
-
-    public ElytraStartFlyingListener(AppConfig appConfig) {
-        this.appConfig = appConfig;
-    }
-
     @Override
     public void accept(PlayerStartFlyingWithElytraEvent event) {
-        event.getPlayer().getInventory().setItemStack(this.appConfig.fireworkBoostSlot(), Items.PLAYER_FIREWORK);
+        event.getPlayer().getInventory().setItemInOffHand(Items.PLAYER_FIREWORK);
+        event.getPlayer().getInventory().update();
     }
 }
