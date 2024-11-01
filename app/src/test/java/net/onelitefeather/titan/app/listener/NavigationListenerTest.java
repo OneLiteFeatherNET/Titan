d3aa87bf009cdb227ae258ad7e4b3ee51ad5701c
Phillipp Glanz <p.glanz@madfix.me>
2024-11-01 14:49:45 +0100

    Add tests for app server

---
+++ app/src/test/java/net/onelitefeather/titan/app/listener/NavigationListenerTest.java
@@ -0,0 +1,35 @@
+package net.onelitefeather.titan.app.listener;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.player.PlayerUseItemEvent;
+import net.minestom.server.instance.Instance;
+import net.minestom.testing.Env;
+import net.minestom.testing.extension.MicrotusExtension;
+import net.onelitefeather.titan.app.helper.NavigationHelper;
+import net.onelitefeather.titan.app.testutils.DummyDeliver;
+import net.onelitefeather.titan.common.utils.Items;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MicrotusExtension.class)
+class NavigationListenerTest {
+
+    @Test
+    @DisplayName("Test has the navigator is opened when the player uses the player teleporter")
+    void testNavigationListenerForClicked(Env env) {
+        NavigationHelper navigationHelper = spy(NavigationHelper.instance(DummyDeliver.instance()));
+        Instance flatInstance = env.createFlatInstance();
+        Player player = env.createPlayer(flatInstance);
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerUseItemEvent.class, new NavigationListener(navigationHelper));
+        PlayerUseItemEvent playerUseItemEvent = new PlayerUseItemEvent(player, Player.Hand.MAIN, Items.PLAYER_TELEPORTER, 1);
+        MinecraftServer.getGlobalEventHandler().call(playerUseItemEvent);
+
+        verify(navigationHelper).openNavigator(player);
+    }
+
+}
