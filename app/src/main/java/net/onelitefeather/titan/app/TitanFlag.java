ed2693bb1f990cca5d33caffc26cd383d095a106
Phillipp Glanz <p.glanz@madfix.me>
2024-11-01 19:14:25 +0100

    Fix healthcheck for agones

--- app/src/main/java/net/onelitefeather/titan/app/TitanFlag.java
+++ app/src/main/java/net/onelitefeather/titan/app/TitanFlag.java
@@ -7,7 +7,7 @@ public final class TitanFlag {
     public static final boolean AGONES_SUPPORT = stringProperty("AGONES_SDK_GRPC_HOST") != null;
 
     private static String stringProperty(@NotNull String name) {
-        return System.getProperty(name);
+        return System.getenv(name);
     }
 
 }
