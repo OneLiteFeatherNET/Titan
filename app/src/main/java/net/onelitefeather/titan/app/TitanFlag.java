5c73668c38692cd14b2f0feb7de955eceab1bc28
Phillipp Glanz <p.glanz@madfix.me>
2024-11-01 19:21:03 +0100

    Fix healthcheck for agones and switch to AGONES_SDK_GRPC_PORT

--- app/src/main/java/net/onelitefeather/titan/app/TitanFlag.java
+++ app/src/main/java/net/onelitefeather/titan/app/TitanFlag.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 public final class TitanFlag {
 
-    public static final boolean AGONES_SUPPORT = stringProperty("AGONES_SDK_GRPC_HOST") != null;
+    public static final boolean AGONES_SUPPORT = stringProperty("AGONES_SDK_GRPC_PORT") != null;
 
     private static String stringProperty(@NotNull String name) {
         return System.getenv(name);
