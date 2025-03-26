d0b3876bd8e4c97c9ed09dbe3ad1159126950c32
Phillipp Glanz <p.glanz@madfix.me>
2025-03-26 21:36:39 +0100

    Update build configuration and enhance MessageChannelDeliver functionality

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,10 +9,13 @@ dependencies {
     api(project(":api"))
     implementation(libs.aves)
 
+    implementation(platform(libs.cloudnet.bom))
+    implementation(libs.cloudnet.jvm.wrapper)
+    implementation(libs.cloudnet.bridge)
+
     testImplementation(platform("org.junit:junit-bom:5.12.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
 tasks.test {
     useJUnitPlatform()
 }
--- common/src/main/java/net/onelitefeather/titan/common/deliver/MessageChannelDeliver.java
+++ common/src/main/java/net/onelitefeather/titan/common/deliver/MessageChannelDeliver.java
@@ -1,32 +1,27 @@
 package net.onelitefeather.titan.common.deliver;
 
+import eu.cloudnetservice.driver.inject.InjectionLayer;
+import eu.cloudnetservice.driver.registry.ServiceRegistry;
+import eu.cloudnetservice.modules.bridge.player.PlayerManager;
+import eu.cloudnetservice.modules.bridge.player.executor.ServerSelectorType;
 import net.minestom.server.entity.Player;
 import net.onelitefeather.deliver.DeliverComponent;
 import net.onelitefeather.titan.api.deliver.Deliver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 
 public final class MessageChannelDeliver implements Deliver {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MessageChannelDeliver.class);
+    private static final ServiceRegistry serviceRegistry = InjectionLayer.ext().instance(ServiceRegistry.class);
+    private static final PlayerManager playerManager = serviceRegistry.firstProvider(PlayerManager.class);
 
     @Override
     public void sendPlayer(Player player, DeliverComponent component) {
-        String channel = switch (component.type()) {
-            case TASK -> "connect:fleet";
-            case SERVER -> "connect:game_server";
+        if (player == null) throw new IllegalArgumentException("player cannot be null");
+        if (component == null) throw new IllegalArgumentException("component cannot be null");
+        switch (component) {
+            case DeliverComponent.TaskComponent taskComponent -> playerManager.playerExecutor(player.getUuid()).connectToTask(taskComponent.taskName(), ServerSelectorType.LOWEST_PLAYERS);
+            case DeliverComponent.ServerDeliverComponent serverDeliverComponent -> playerManager.playerExecutor(player.getUuid()).connect(serverDeliverComponent.gameServer());
             case null, default -> throw new IllegalStateException("Unexpected value: " + component.type());
         };
-        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-             ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
-            objectOutputStream.writeObject(component);
-            player.sendPluginMessage(channel, byteArrayOutputStream.toByteArray());
-        } catch (IOException e) {
-            LOGGER.error("Failed to send player message", e);
-        }
+
     }
 }
