5f9ff3ab79f5cb6213800182956fa644477a6c5a
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-07-09 18:52:04 +0200

    fix(naming): rename ArgumentMaterialType package from commands to argument

--- common/src/main/java/net/onelitefeather/titan/common/argument/ArgumentMaterialType.java
+++ common/src/main/java/net/onelitefeather/titan/common/argument/ArgumentMaterialType.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.onelitefeather.titan.common.commands;
+package net.onelitefeather.titan.common.argument;
 
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.arguments.ArgumentString;
