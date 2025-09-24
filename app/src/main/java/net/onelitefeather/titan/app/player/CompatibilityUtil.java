c10fc93e8cbad1464c8232ca5c7223ebc3999741
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-09-24 10:29:03 +0200

    chore: add Apache License 2.0 header to CompatibilityUtil and TitanPlayer

--- app/src/main/java/net/onelitefeather/titan/app/player/CompatibilityUtil.java
+++ app/src/main/java/net/onelitefeather/titan/app/player/CompatibilityUtil.java
@@ -1,10 +1,25 @@
+/**
+ * Copyright 2025 OneLiteFeather Network
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.onelitefeather.titan.app.player;
 
 import net.kyori.adventure.util.TriState;
 import net.luckperms.api.util.Tristate;
 
 final class CompatibilityUtil {
-    private  CompatibilityUtil() {
+    private CompatibilityUtil() {
     }
 
     static TriState convertTriState(Tristate tristate) {
