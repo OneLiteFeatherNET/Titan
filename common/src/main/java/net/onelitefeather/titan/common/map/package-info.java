69bc2bd4b92aeefc9c03c70ac7b9374f23c6eabc
TheMeinerLP <github@themeinerlp.dev>
2026-08-28 10:26:23 +0200

    refactor(map): declare the map package non-null by default
    Adds the package-info the OLF Minestom standard asks for (OLF-L3-01) and
    removes the hand-placed @NotNull annotations it makes redundant, starting
    with MapEntry, which also gains the type and method javadoc of OLF-L4-01.
    
    The remaining classes of the package follow in the commits that change them.

---
+++ common/src/main/java/net/onelitefeather/titan/common/map/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2025 OneLiteFeather Network
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+/**
+ * Holds the lobby worlds: which one is selected, where its map data lives and which engine serves
+ * its chunks.
+ *
+ * @author theEvilReaper
+ * @author TheMeinerLP
+ * @version 1.0.0
+ * @since 1.15.0
+ */
+@NotNullByDefault
+package net.onelitefeather.titan.common.map;
+
+import org.jetbrains.annotations.NotNullByDefault;
